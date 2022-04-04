f5466842bc4926167630a6ab86efad9f705f9ff1
hasibmiraz <hasibmiraz09@gmail.com>
2022-04-05 04:00:24 +0600

    Home page text padding edited

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,11 +6,11 @@ const Home = () => {
     'https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80';
   return (
     <div>
-      <div className="px-20 flex flex-col lg:flex-row my-8">
+      <div className="px-4 lg:px-20 flex flex-col lg:flex-row my-8">
         <div className="details-container flex-1">
           <div className="flex justify-center items-center h-full">
-            <div className="px-8">
-              <h1 className="text-6xl font-bold text-blue-500 border-l-2 border-blue-200 pl-4">
+            <div className="px-2 lg:px-8">
+              <h1 className="text-3xl lg:text-6xl font-bold text-blue-500 border-l-2 border-blue-200 pl-4">
                 Taste Our Food
               </h1>
               <p className="px-4 mt-4 text-gray-500 text-lg">
