62a30c4a09fbfa2553e95de224415a752eb6ecfb
hasibmiraz <hasibmiraz09@gmail.com>
2022-04-05 14:08:11 +0600

    Dashboard chart added

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,9 +1,92 @@
 import React from 'react';
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from 'recharts';
 
 const Dashboard = () => {
+  const data = [
+    {
+      month: 'Mar',
+      investment: 100000,
+      sell: 241,
+      revenue: 10401,
+    },
+    {
+      month: 'Apr',
+      investment: 200000,
+      sell: 423,
+      revenue: 24500,
+    },
+    {
+      month: 'May',
+      investment: 500000,
+      sell: 726,
+      revenue: 67010,
+    },
+    {
+      month: 'Jun',
+      investment: 500000,
+      sell: 529,
+      revenue: 40405,
+    },
+    {
+      month: 'Jul',
+      investment: 600000,
+      sell: 601,
+      revenue: 50900,
+    },
+    {
+      month: 'Aug',
+      investment: 700000,
+      sell: 670,
+      revenue: 61000,
+    },
+  ];
+
   return (
-    <div>
-      <h1>This is dashboard</h1>
+    <div className="grid grid-cols-1 lg:grid-cols-2">
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="text-4xl my-4">Monthly Investment VS Revenue</h1>
+        <BarChart
+          width={700}
+          height={600}
+          data={data}
+          margin={{
+            top: 20,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="month" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="revenue" stackId="a" fill="#8884d8" />
+          <Bar dataKey="investment" stackId="a" fill="#82ca9d" />
+        </BarChart>
+      </div>
+      <div>
+        <div className="flex flex-col justify-center items-center">
+          <h1 className="text-4xl my-4">Monthly Investment</h1>
+          <LineChart width={700} height={600} data={data}>
+            <Line type="monotone" dataKey="sell" stroke="#8884d8" />
+            <CartesianGrid stroke="#ccc" />
+            <Tooltip />
+            <XAxis dataKey="month" />
+            <YAxis />
+          </LineChart>
+        </div>
+      </div>
     </div>
   );
 };
